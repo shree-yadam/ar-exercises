d37f60a98106615e9494e4406b617a1fc555742e
Shree Devi Yadam <shree.yadam@gmail.com>
2021-07-19 22:05:17 +0000

    Implement and test Excercise 1

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,6 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "Total Revenue for all stores: #{Store.all.sum(:annual_revenue)}"
+puts "Average annual revenue for all stores: #{Store.all.sum(:annual_revenue)/Store.count}"
+puts "Number of stores that are generating $1M or more in annual sales: #{Store.where('annual_revenue >= 1000000').count}"
