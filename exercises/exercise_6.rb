52fd8cc0cf74bf411d8a41d51246a0a17480f2e7
Shree Devi Yadam <shree.yadam@gmail.com>
2021-07-19 23:09:32 +0000

    Add excercise 7 and test cases

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Guess", last_name: "Name", hourly_rate: 60)
+@store1.employees.create(first_name: "Crush", last_name: "It", hourly_rate: 50)
+@store1.employees.create(first_name: "Employ", last_name: "Me", hourly_rate: 70)
+@store2.employees.create(first_name: "ABC", last_name: "DEF", hourly_rate: 60)
+@store2.employees.create(first_name: "GHI", last_name: "JKL", hourly_rate: 50)
+@store2.employees.create(first_name: "MNO", last_name: "PQR", hourly_rate: 60)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,58 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "TEST CASE 1:"
+puts "Enter store name: "
+store_name = gets.chomp
+store = Store.create(name: store_name)
+store.errors.each do |field, error|
+  puts "#{field} => #{error}"
+end
+
+puts "TEST CASE 2:"
+@store1.employees.create(last_name: "VWX", hourly_rate: 200).errors.each do |field, error|
+  puts "#{field} => #{error}"
+end
+
+puts "TEST CASE 3:"
+@store1.employees.create(first_name: "VWX", hourly_rate: 200).errors.each do |field, error|
+  puts "#{field} => #{error}"
+end
+
+puts "TEST CASE 4:"
+@store1.employees.create(first_name: "STU", last_name: "VWX").errors.each do |field, error|
+  puts "#{field} => #{error}"
+end
+
+puts "TEST CASE 5:"
+@store1.employees.create(first_name: "STU", last_name: "VWX", hourly_rate: 2000).errors.each do |field, error|
+  puts "#{field} => #{error}"
+end
+
+puts "TEST CASE 6:"
+@store1.employees.create(first_name: "STU", last_name: "VWX", hourly_rate: 20).errors.each do |field, error|
+  puts "#{field} => #{error}"
+end
+
+puts "TEST CASE 7:"
+Employee.create(first_name: "STU", last_name: "VWX", hourly_rate: 40).errors.each do |field, error|
+  puts "#{field} => #{error}"
+end
+
+puts "TEST CASE 8:"
+store = Store.create(name: "A", annual_revenue: 20)
+store.errors.each do |field, error|
+  puts "#{field} => #{error}"
+end
+
+puts "TEST CASE 9:"
+store = Store.create(name: "A", annual_revenue: "B")
+store.errors.each do |field, error|
+  puts "#{field} => #{error}"
+end
+
+puts "TEST CASE 10:"
+store = Store.create(name: "A", annual_revenue: -2)
+store.errors.each do |field, error|
+  puts "#{field} => #{error}"
+end
