2e1dc3e20038a3bb5ac79ada57703b4fb3070d63
Shree Devi Yadam <shree.yadam@gmail.com>
2021-07-20 03:09:13 +0000

    Implement excercise 9 and before_destroy callback in store

--- lib/store.rb
+++ lib/store.rb
@@ -1,5 +1,6 @@
 class Store < ActiveRecord::Base
 
+
   has_many :employees
 
   validates :name, presence: true, length: { minimum: 3 }
@@ -8,12 +9,21 @@ class Store < ActiveRecord::Base
 
   validate :atleast_either_mens_or_womens_apparel
 
+  before_destroy :check_empty, prepend: true
+
   private
 
   def atleast_either_mens_or_womens_apparel
-    if (womens_apparel != true) && (mens_apparel != true)
+    if womens_apparel != true && mens_apparel != true
       errors.add(:mens_apparel, "Stores must carry at least one of the men's or women's apparel")
       errors.add(:womens_apparel, "Stores must carry at least one of the men's or women's apparel")
+    end
+  end
+
+  def check_empty
+    if self.employees.size != 0
+      errors.add(:base, "Cannot destroy non empty store")
+      return false;
     end
   end
 
